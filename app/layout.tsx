14f8ec790949cafba5f996aced3174e92b1b7574
Saumy <advertisementgram@gmail.com>
2026-04-26 15:16:35 +0530

    describe your change

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8735497325725812"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className="bg-white text-black dark:bg-black dark:text-white transition-colors">
         {children}
       </body>
